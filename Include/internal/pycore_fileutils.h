a381a0881722ff1aa1135c453e5fd87ebfb69857
Christoph Reiter <reiter.christoph@gmail.com>
2021-06-17 18:52:18 +0530

    fileutils: fix isselectable on windows
    Co-authored-by: Алексей <alexey.pawlow@gmail.com>

--- Include/internal/pycore_fileutils.h
+++ Include/internal/pycore_fileutils.h
@@ -12,7 +12,7 @@ extern "C" {
 
 
 /* A routine to check if a file descriptor can be select()-ed. */
-#ifdef _MSC_VER
+#ifdef MS_WINDOWS
     /* On Windows, any socket fd can be select()-ed, no matter how high */
     #define _PyIsSelectable_fd(FD) (1)
 #else
