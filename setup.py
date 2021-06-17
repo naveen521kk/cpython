7b53099a40c40b54ecf100139d242aabb235e0ba
Christoph Reiter <reiter.christoph@gmail.com>
2021-06-17 18:52:11 +0530

    build: build winconsoleio and _testconsole
    Co-authored-by: Алексей <alexey.pawlow@gmail.com>

--- setup.py
+++ setup.py
@@ -1094,6 +1094,9 @@ class PyBuildExt(build_ext):
                            ['_xxtestfuzz/_xxtestfuzz.c',
                             '_xxtestfuzz/fuzzer.c']))
 
+        if MS_WINDOWS:
+            self.add(Extension('_testconsole', ['../PC/_testconsole.c']))
+
     def detect_readline_curses(self):
         # readline
         readline_termcap_library = ""
