347d8e4b1dd5e783b02a8310495a613a22ac17ae
Naveen M K <naveen521kk@gmail.com>
2021-06-23 18:12:12 +0530

    Fix extension suffix for c-extensions on mingw
    Python is compiled with various compilers which previously
    had same platform tags or extension suffix. This can be error
    prone while loading c-extensions, so now each compiler or
    runtime has a different extension suffix.
    
    Also, changed all extension to end with .pyd rather than
    .dll file.
    
    Fixes https://github.com/msys2/MINGW-packages/issues/8843
    
    Signed-off-by: Naveen M K <naveen521kk@gmail.com>

--- Python/dynload_win.c
+++ Python/dynload_win.c
@@ -27,12 +27,6 @@
 #define PYD_UNTAGGED_SUFFIX PYD_DEBUG_SUFFIX ".pyd"
 
 const char *_PyImport_DynLoadFiletab[] = {
-#ifdef EXT_SUFFIX
-    EXT_SUFFIX, /* include SOABI flags where is encoded debug */
-#endif
-#ifdef SHLIB_SUFFIX
-    "-abi" PYTHON_ABI_STRING SHLIB_SUFFIX,
-#endif
     PYD_TAGGED_SUFFIX,
     PYD_UNTAGGED_SUFFIX,
     NULL
